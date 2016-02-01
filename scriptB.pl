b2b32d61505187bc11da79565f082e9a5afcff83
Foolson <johan.olsson.91@gmail.com>
2016-02-01 10:28:50 +0100

    Added the metadata header to all scripts

--- scriptB.pl
+++ scriptB.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl
+################## METADATA ##################
+# NAME: Johan Olsson
+# USERNAME: d15johol
+# COURSE: IT341G VT16: Scriptprogrammering G1F, 7.5hp
+# ASSIGNMENT: Script B
+# DATE OF LAST CHANGE: 2016-02-01
+##############################################
 
 use warnings;
 use strict;
--- scriptD.pl
+++ scriptD.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl
+################## METADATA ##################
+# NAME: Johan Olsson
+# USERNAME: d15johol
+# COURSE: IT341G VT16: Scriptprogrammering G1F, 7.5hp
+# ASSIGNMENT: Script D
+# DATE OF LAST CHANGE: 2016-02-01
+##############################################
 
 use warnings;
 use strict;
@@ -13,7 +20,8 @@ close FH or die "Can't close $file: $!";
 @users = sort { lc($a) cmp lc($b) } @users;
 
 open FH, ">", "$newFile" or die "Can't open $newFile: $!";
-while (my $test = <@users>) {
-    print FH "$test\n";
+while (my $line = <@users>) {
+    chomp $line;
+    print FH "$line\n";
 }
 close FH or die "Can't close $newFile: $!";
