67a337d945268ddef6e84e03d4fe1606f1c8e27f
Foolson <johan.olsson.91@gmail.com>
2016-02-02 14:11:45 +0100

    The user monitor script can now list users and their logins

--- user.pl
+++ user.pl
@@ -3,28 +3,33 @@
 use warnings;
 use strict;
 
-my $user = "root";
-
-my @users;
-
-my @last = `last $user`;
-
-my @logins = @last;
-
-print "$user - $#logins\n";
-
 my $f;
 
 $f = "/etc/login.defs";
-
 open FH, "<", "$f" or die "Can't open $f: $!";
 my @UID;
 while( my $line = <FH> ) {
-  if ( /GID_MIN/ ) {
-    chomp $line;
+  if ( $line =~ /^(UID_M\w{2}\s+)\d+/ ) {
+    $line =~ s/\D+//;
     push @UID, $line;
   }
 }
 close FH or die "Can't close $f: $!";
 
-print @UID;
+my $firstUID = $UID[0] - 1;
+my $lastUID = $UID[1] + 1;
+my @passwd = `getent passwd | awk -F: '$firstUID<\$3 && \$3<$lastUID {print \$1}'`;
+my @users = @passwd;
+chomp @users;
+
+my %usersLogins;
+for my $i (0 ... $#users) {
+  my $user = $users[$i];
+  my @last = `last $user`;
+  my @logins = @last;
+  $usersLogins{$user} = ($#logins - 1);
+}
+
+foreach my $key (sort {lc $a cmp lc $b} keys %usersLogins) {
+  printf $key.":".$usersLogins{$key}."\n";
+}
