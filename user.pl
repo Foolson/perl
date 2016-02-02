5c665c2c9f5b9efe91be68f2f8f2b781d25c2085
Foolson <johan.olsson.91@gmail.com>
2016-02-02 10:24:31 +0100

    Stuff

--- user.pl
+++ user.pl
@@ -5,8 +5,26 @@ use strict;
 
 my $user = "root";
 
+my @users;
+
 my @last = `last $user`;
 
 my @logins = @last;
 
 print "$user - $#logins\n"
+
+my $f;
+
+$f = "/etc/login.defs";
+
+open FH, "<", "$f" or die "Can't open $f: $!";
+my @UID;
+while( my $line = <FH> ) {
+  if ( /GID_MIN/ ) {
+    chomp $line;
+    push @UID, $line;
+  }
+}
+close FH or die "Can't close $f: $!";
+
+print @UID;
