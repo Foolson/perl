c5d1e03bdda6d2219669a6a2f95fb1560ee9f506
Foolson <johan.olsson.91@gmail.com>
2016-02-17 02:51:46 -0500

    The monitor script is working as a test and can use the user module, YIPPIE

---
+++ monitor.pl
@@ -0,0 +1,23 @@
+use warnings;
+use strict;
+
+use userModule;
+
+print "\# User Storage\n";
+print "Size: ";
+my $size = <>;
+my %userStorage = userModule::userStorage($size);
+print %userStorage;
+print "\n\n";
+
+print "\# Password Age\n";
+print "Days: ";
+my $days = <>;
+my %passwordAge = userModule::passwordAge($days);
+print %passwordAge;
+print "\n\n";
+
+print "\# User Logins\n";
+my %userLogins = userModule::userLogins();
+print %userLogins;
+print "\n";
