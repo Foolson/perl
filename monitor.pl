cffbe9220aacdecfd19e3e595f57c448875eed47
Foolson <johan.olsson.91@gmail.com>
2016-02-23 16:55:10 +0100

    Sort of done, mail is only printing has to send it somwhere

--- monitor.pl
+++ monitor.pl
@@ -6,6 +6,7 @@ use userModule;
 use systemModule;
 use Data::Dumper;
 use Text::CSV;
+use MIME::Entity;
 ###
 # Subrutines
 ###
@@ -38,18 +39,25 @@ my @network;
 my $networkColumns = "portNumber listeningDevice processName";
 # Network tcp ipv4
 @network = @{systemModule::network("tcp ipv4")};
-createCSV("/root/tcp-ipv4.csv",convertAoh($networkColumns, @network));
+createCSV("/tmp/tcp-ipv4.csv",convertAoh($networkColumns, @network));
 # Network tcp ipv6
 @network = @{systemModule::network("tcp ipv6")};
-createCSV("/root/tcp-ipv6.csv",convertAoh($networkColumns, @network));
+createCSV("/tmp/tcp-ipv6.csv",convertAoh($networkColumns, @network));
 # Network udp ipv4
 @network = @{systemModule::network("udp ipv4")};
-createCSV("/root/udp-ipv4.csv",convertAoh($networkColumns, @network));
+createCSV("/tmp/udp-ipv4.csv",convertAoh($networkColumns, @network));
 # Network udp ipv6
 @network = @{systemModule::network("udp ipv6")};
-createCSV("/root/udp-ipv6.csv",convertAoh($networkColumns, @network));
+createCSV("/tmp/udp-ipv6.csv",convertAoh($networkColumns, @network));
 # Service status
 my @serviceQuery = ("fprintd","firewalld","sshd","named","httpd","cups");
+my @serviceStatus;
+for my $i (0...$#serviceQuery) {
+  my $status = (systemModule::serviceStatus($serviceQuery[$i]) ? "yes" : "no");
+  my @array = ("$serviceQuery[$i]", $status);
+  push @serviceStatus, \@array;
+}
+createCSV("/tmp/serviceStatus.csv",@serviceStatus);
 # User data
 my %userLogins = %{userModule::userLogins()};
 my %passwordAge = %{userModule::passwordAge(100)};
@@ -60,5 +68,36 @@ for my $i (0...$#users) {
   my @array = ("$users[$i]","$userLogins{$users[$i]}","$passwordAge{$users[$i]}","$userStorage{$users[$i]}");
   push @userData, \@array;
 }
-print Dumper @userData;
-createCSV("/root/userData.csv",@userData);
+createCSV("/tmp/userData.csv",@userData);
+###
+# Send E-Mail
+###
+my $top;
+my $time = time();
+$top = MIME::Entity->build(From     => 'root@centos.local',
+                           To       => 'root@centos.local',
+                           Subject  => "taskC mail at epoch $time",
+                           Boundary => "==== taskC ==== epoch=$time ====",
+                           Type     => "multipart/mixed",
+                           Charset  => "us-ascii");
+
+$top->attach(Path     => "/tmp/tcp-ipv4.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+$top->attach(Path     => "/tmp/tcp-ipv6.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+$top->attach(Path     => "/tmp/udp-ipv4.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+$top->attach(Path     => "/tmp/udp-ipv6.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+$top->attach(Path     => "/tmp/serviceStatus.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+$top->attach(Path     => "/tmp/userData.csv",
+             Type     => "text/csv",
+             Encoding => "quoted-printable");
+
+$top->print;
